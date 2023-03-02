bb4831a6ac8cc8a9b50c3fbc42e3a346301f7b9b
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-03-02 11:55:18 -0600

    implemented Date

--- Date.java
+++ Date.java
@@ -1,4 +1,47 @@
 public class Date {
+
+    private int month;
+    private int day;
+    private int year;
+
+    public Date(int dateMonth, int dateDay, int dateYear) {
+        month = dateMonth;
+        day = dateDay;
+        year = dateYear;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getDay() {
+        return day;
+    } 
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        String result = "";
+        if (month < 10) {
+            result = result + 0 + month + "/";
+        } else {
+            result = result + month + "/";
+        }
+        if (day < 10) {
+            result = result + 0 + day + "/";
+        } else {
+            result = result + day + "/";
+        }
+        if (year < 10) {
+            result = result + 0 + 0 + 0 + year;
+        } else if (year < 100) {
+            result = result + 0 + 0 + year;
+        } else if (year < 1000) {
+            result = result + 0 + year;
+        } else {
+            result = result + year;
+        }
+        return result;
+    }
     public static void main(String[] args) {
         
     }
